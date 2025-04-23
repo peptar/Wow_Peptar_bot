e47eef330be0cc8167b9d48278570de790d7166e
peptar <okeeper@gmail.com>
2025-04-23 19:23:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,41 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import logging
 import os
 
-API_TOKEN = os.getenv('API_TOKEN')  # Токен из переменной окружения
+# Получаем токен из переменной окружения
+API_TOKEN = os.getenv('API_TOKEN')
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Клавиатуры
+# Клавиатура: стартовая
 start_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-start_kb.add(KeyboardButton(\"Мой персонаж 80-го уровня\"))
+start_kb.add(KeyboardButton("Мой персонаж 80-го уровня"))
 
+# Клавиатура: вопрос про ilvl
 goto_ilvl_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-goto_ilvl_kb.add(KeyboardButton(\"Окей, какой у тебя ilvl?\"))
+goto_ilvl_kb.add(KeyboardButton("Окей, какой у тебя ilvl?"))
 
+# Команда /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer(\"Привет!\", reply_markup=start_kb)
+    await message.answer("Привет!", reply_markup=start_kb)
 
-@dp.message_handler(lambda message: message.text == \"Мой персонаж 80-го уровня\")
+# Пользователь говорит, что у него персонаж 80-го уровня
+@dp.message_handler(lambda message: message.text == "Мой персонаж 80-го уровня")
 async def check_level(message: types.Message):
-    await message.answer(\"Окей, какой у тебя ilvl?\", reply_markup=goto_ilvl_kb)
+    await message.answer("Окей, какой у тебя ilvl?", reply_markup=goto_ilvl_kb)
 
-@dp.message_handler(lambda message: message.text == \"Окей, какой у тебя ilvl?\")
+# Пользователь отвечает про ilvl
+@dp.message_handler(lambda message: message.text == "Окей, какой у тебя ilvl?")
 async def ask_ilvl(message: types.Message):
-    await message.answer(\"Готово\", reply_markup=start_kb)
+    await message.answer("Готово", reply_markup=start_kb)
 
+# Обработка всего остального
 @dp.message_handler()
 async def fallback(message: types.Message):
-    await message.answer(\"Пожалуйста, используй кнопки ниже.\", reply_markup=start_kb)
+    await message.answer("Пожалуйста, используй кнопки ниже.", reply_markup=start_kb)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
